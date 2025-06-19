9ed9cdc1d55ce5abc7fde8e3504ed1b3dd182886
LbStoyanov <!Ymm%PkM0g%clYso@5!B9>
2025-06-19 15:15:29 +0300

    Adding logic for element key check

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -4,22 +4,26 @@ import AddCategory from "./components/AddCategory";
 
 const GifExpertApp = () => {
 
-      const [categories, setCategories] = useState([ 'One Punch', 'Dragon Ball' ])
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
 
-      const onAddCategory = ( newCategory ) => {
-        
-        setCategories( [...categories, newCategory] );
-      }
-      
+  const onAddCategory = (newCategory) => {
+
+    if(categories.includes(newCategory)) return
+
+    setCategories([...categories, newCategory]);
+  }
+
 
   return (
     <>
       <h1>GifExpertApp</h1>
 
-      <AddCategory addCategory={ onAddCategory }/>
+      <AddCategory
+        addCategory={onAddCategory}
+      />
 
       <ol>
-        { categories.map( c => { return <li key={ c }>{ c }</li>} ) }        
+        {categories.map(c => { return <li key={c}>{c}</li> })}
       </ol>
 
     </>
